27b88353e40b3b1339895ff7acb5146f8742b553
Nora Liu <tzuyuanliu0830@gmail.com>
2020-05-24 22:54:03 +0800

    add usdc

--- src/utils/erc20List.js
+++ src/utils/erc20List.js
@@ -26,6 +26,13 @@ const ERC20List = [
 		unit: 18,
 		contractAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
 		rateId: 'dai'
+	},
+	{
+		name: 'USD//C',
+		symbol: 'USDC',
+		unit: 6,
+		contractAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+		rateId: 'usd-coin'
 	}
 ];
 
