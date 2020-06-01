1d7d537c48acf9c3e7c400f799c182a96707a304
Nora Liu <tzuyuanliu0830@gmail.com>
2020-06-01 20:12:22 +0800

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Main, Header, Button } from '@aragon/ui'
-import MakeOrder from './pages/MakeOrder'
-import ConnectButton from './components/NavBar/ConnectButton'
-
+import React from 'react';
+import { Main, Header, Button } from '@aragon/ui';
+import MakeOrder from './pages/MakeOrder';
+import ConnectButton from './components/NavBar/ConnectButton';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 export default function App() {
-  return (
-    <Main>
-      <Header
-        primary="Opyn TaCo"
-        secondary={<ConnectButton />}
-      />
-      <MakeOrder />
-    </Main>
-  )
+	return (
+		<Router>
+			<Main>
+				<Header primary="Opyn TaCo" secondary={<ConnectButton />} />
+				<Route path="/">
+					<MakeOrder />
+				</Route>
+			</Main>
+		</Router>
+	);
 }
